2b1de6978231bc01afc72411118b7bc089fb2d8f
Cem <22315@ma-web.nl>
2017-11-29 21:19:03 +0100

    deleted file

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-        define('HOST','localhost');
-        define('USER','22683_root');
-        define('PASS','abc123');
-        define('DBNAME','22683_instaclone');
-
-        $baseSite = 'http://22683.hosts.ma-cloud.nl/bewijzenmap/p1.3/proj/index.php';
-        ?>
